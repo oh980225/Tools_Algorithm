2542e6d859d63c1dae16c9d9b72bc9fb7298a5fe
oh980225 <oh980225@gmail.com>
2021-04-04 21:40:29 +0900

    Solve 2589

--- Week2/Week2/1057.cpp
+++ Week2/Week2/1057.cpp
@@ -1,19 +1,19 @@
-#include <bits/stdc++.h>
-
-using namespace std;
-
-int n, a, b;
-
-int main() {
-	scanf_s("%d %d %d", &n, &a, &b);
-	int ans = 0;
-	while (a != b) {
-		a -= a / 2;
-		b -= b / 2;
-		ans++;
-	}
-
-	printf("%d", ans);
-
-	return 0;
-}
+//#include <bits/stdc++.h>
+//
+//using namespace std;
+//
+//int n, a, b;
+//
+//int main() {
+//	scanf_s("%d %d %d", &n, &a, &b);
+//	int ans = 0;
+//	while (a != b) {
+//		a -= a / 2;
+//		b -= b / 2;
+//		ans++;
+//	}
+//
+//	printf("%d", ans);
+//
+//	return 0;
+//}
