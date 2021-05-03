e7c91802a587fa87244313f9aacccc6751b847d6
oh980225 <oh980225@gmail.com>
2021-05-04 02:22:22 +0900

    Solve 2606, 카펫(programmers)

--- Programmers/Programmers/소수찾기.cpp
+++ Programmers/Programmers/소수찾기.cpp
@@ -1,78 +1,78 @@
-#include <string>
-#include <vector>
-#include <iostream>
-
-using namespace std;
-
-int ans;
-int arr[7];
-int chk[7];
-bool checkNum[10000000];
-string str;
-
-bool isPrime(int num) {
-    if (num < 2)
-        return false;
-
-    int a = (int)sqrt(num);
-
-    for (int i = 2; i <= a; i++) {
-        if (num % i == 0)
-            return false;
-    }
-
-    return true;
-}
-
-
-void dfs(int cnt, int n) {
-    if (cnt == n) {
-        int number = 0;
-
-        for (int i = 1; i <= n; i++) {
-            number += arr[i - 1] * pow(10, n - i);
-        }
-
-        if (!checkNum[number]) {
-            checkNum[number] = true;
-
-            if (isPrime(number))
-                ans++;
-        }
-
-        return;
-    }
-
-    for (int i = 0; i < str.size(); i++) {
-        if (!chk[i]) {
-            chk[i] = true;
-            arr[cnt] = str[i] - '0';
-            dfs(cnt + 1, n);
-            chk[i] = false;
-        }
-    }
-
-    return;
-}
-
-
-int solution(string numbers) {
-    int answer = 0;
-    str = numbers;
-
-    for (int i = 1; i <= numbers.size(); i++) {
-        dfs(0, i);
-    }
-
-    answer = ans;
-
-    return answer;
-}
-
-int main() {
-    cin >> str;
-
-    cout << solution(str);
-
-    return 0;
-}
+//#include <string>
+//#include <vector>
+//#include <iostream>
+//
+//using namespace std;
+//
+//int ans;
+//int arr[7];
+//int chk[7];
+//bool checkNum[10000000];
+//string str;
+//
+//bool isPrime(int num) {
+//    if (num < 2)
+//        return false;
+//
+//    int a = (int)sqrt(num);
+//
+//    for (int i = 2; i <= a; i++) {
+//        if (num % i == 0)
+//            return false;
+//    }
+//
+//    return true;
+//}
+//
+//
+//void dfs(int cnt, int n) {
+//    if (cnt == n) {
+//        int number = 0;
+//
+//        for (int i = 1; i <= n; i++) {
+//            number += arr[i - 1] * pow(10, n - i);
+//        }
+//
+//        if (!checkNum[number]) {
+//            checkNum[number] = true;
+//
+//            if (isPrime(number))
+//                ans++;
+//        }
+//
+//        return;
+//    }
+//
+//    for (int i = 0; i < str.size(); i++) {
+//        if (!chk[i]) {
+//            chk[i] = true;
+//            arr[cnt] = str[i] - '0';
+//            dfs(cnt + 1, n);
+//            chk[i] = false;
+//        }
+//    }
+//
+//    return;
+//}
+//
+//
+//int solution(string numbers) {
+//    int answer = 0;
+//    str = numbers;
+//
+//    for (int i = 1; i <= numbers.size(); i++) {
+//        dfs(0, i);
+//    }
+//
+//    answer = ans;
+//
+//    return answer;
+//}
+//
+//int main() {
+//    cin >> str;
+//
+//    cout << solution(str);
+//
+//    return 0;
+//}
--- Week3/Week3/2667.cpp
+++ Week3/Week3/2667.cpp
@@ -1,56 +1,56 @@
-#include <bits/stdc++.h>
-
-using namespace std;
-
-int n;
-string matrix[30];
-bool chk[30][30];
-vector<int> ans;
-int cnt;
-
-struct MOVE {
-	int x, y;
-};
-
-MOVE moveArr[4] = { {0, 1}, {1, 0}, {-1, 0}, {0, -1} };
-
-void dfs(int x, int y) {
-	cnt++;
-
-	for (int i = 0; i < 4; i++) {
-		int nX = x + moveArr[i].x;
-		int nY = y + moveArr[i].y;
-		if (0 <= nX && nX < n && 0 <= nY && nY < n && matrix[nY][nX]-'0' == 1) {
-			matrix[nY][nX] = '0';
-			dfs(nX, nY);
-		}
-	}
-
-	return;
-}
-
-int main() {
-	cin >> n;
-	for (int i = 0; i < n; i++) {
-		cin >> matrix[i];
-	}
-
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			if (matrix[i][j] - '0' == 1) {
-				cnt = 0;
-				matrix[i][j] = '0';
-				dfs(j, i);
-				ans.push_back(cnt);
-			}
-		}
-	}
-
-	cout << ans.size() << endl;
-	sort(ans.begin(), ans.end());
-	for (int i = 0; i < ans.size(); i++) {
-		cout << ans[i] << endl;
-	}
-
-	return 0;
-}
+//#include <bits/stdc++.h>
+//
+//using namespace std;
+//
+//int n;
+//string matrix[30];
+//bool chk[30][30];
+//vector<int> ans;
+//int cnt;
+//
+//struct MOVE {
+//	int x, y;
+//};
+//
+//MOVE moveArr[4] = { {0, 1}, {1, 0}, {-1, 0}, {0, -1} };
+//
+//void dfs(int x, int y) {
+//	cnt++;
+//
+//	for (int i = 0; i < 4; i++) {
+//		int nX = x + moveArr[i].x;
+//		int nY = y + moveArr[i].y;
+//		if (0 <= nX && nX < n && 0 <= nY && nY < n && matrix[nY][nX]-'0' == 1) {
+//			matrix[nY][nX] = '0';
+//			dfs(nX, nY);
+//		}
+//	}
+//
+//	return;
+//}
+//
+//int main() {
+//	cin >> n;
+//	for (int i = 0; i < n; i++) {
+//		cin >> matrix[i];
+//	}
+//
+//	for (int i = 0; i < n; i++) {
+//		for (int j = 0; j < n; j++) {
+//			if (matrix[i][j] - '0' == 1) {
+//				cnt = 0;
+//				matrix[i][j] = '0';
+//				dfs(j, i);
+//				ans.push_back(cnt);
+//			}
+//		}
+//	}
+//
+//	cout << ans.size() << endl;
+//	sort(ans.begin(), ans.end());
+//	for (int i = 0; i < ans.size(); i++) {
+//		cout << ans[i] << endl;
+//	}
+//
+//	return 0;
+//}
