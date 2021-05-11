797d7d925abea4aa3b533ac5e56c18764b6edb94
oh980225 <oh980225@gmail.com>
2021-05-11 16:13:42 +0900

    Solve 7576, 베스트앨범(programmers)

---
+++ Programmers/Programmers/베스트앨범.cpp
@@ -0,0 +1,60 @@
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+map<string, vector<pair<int, int>>> m;
+
+bool compare(pair<string, int> a, pair<string, int> b) {
+    return a.second > b.second;
+}
+
+bool compare2(pair<int, int> a, pair<int, int>b) {
+    return a.first > b.first;
+}
+
+vector<int> solution(vector<string> genres, vector<int> plays) {
+    vector<int> answer;
+
+    for (int i = 0; i < genres.size(); i++) {
+        m[genres[i]].push_back(make_pair(plays[i], i));
+    }
+
+    map<string, vector<pair<int, int>>>::iterator it;
+    vector<pair<string, int>> temp;
+    for (it = m.begin(); it != m.end(); it++) {
+        int total = 0;
+        for (auto element : it->second) {
+            total += element.first;
+        }
+        temp.push_back(make_pair(it->first, total));
+    }
+
+    sort(temp.begin(), temp.end(), compare);
+
+    for (auto element : temp) {
+        sort(m[element.first].begin(), m[element.first].end(), compare2);
+        answer.push_back(m[element.first][0].second);
+        if (m[element.first].size() > 1) {
+            answer.push_back(m[element.first][1].second);
+        }
+    }
+
+    return answer;
+}
+
+int main() {
+    vector<string> genres = { "classic", "pop", "classic", "classic", "pop" };
+    vector<int> plays = { 500, 600, 150, 800, 2500 };
+    
+    vector<int> answer = solution(genres, plays);
+
+    for (auto element : answer) {
+        cout << element << ", ";
+    }
+    
+    return 0;
+}
--- Week3/Week3/2606.cpp
+++ Week3/Week3/2606.cpp
@@ -1,48 +1,48 @@
-#include <bits/stdc++.h>
-
-using namespace std;
-
-int ans, n, m;
-bool chk[105];
-vector<int> grp[105];
-
-void bfs(int n) {
-	queue<int> q;
-	chk[n] = true;
-	for (auto element : grp[n]) {
-		if (!chk[element]) {
-			chk[element] = true;
-			q.push(element);
-		}
-	}
-
-	while (q.size()) {
-		int f = q.front();
-		ans++;
-		q.pop();
-		for (auto element : grp[f]) {
-			if (!chk[element]) {
-				chk[element] = true;
-				q.push(element);
-			}
-		}
-	}
-}
-
-int main() {
-	cin >> n;
-	cin >> m;
-	
-	int f, s;
-	for (int i = 0; i < m; i++) {
-		cin >> f >> s;
-		grp[f].push_back(s);
-		grp[s].push_back(f);
-	}
-
-	bfs(1);
-
-	cout << ans;
-
-	return 0;
-}
+//#include <bits/stdc++.h>
+//
+//using namespace std;
+//
+//int ans, n, m;
+//bool chk[105];
+//vector<int> grp[105];
+//
+//void bfs(int n) {
+//	queue<int> q;
+//	chk[n] = true;
+//	for (auto element : grp[n]) {
+//		if (!chk[element]) {
+//			chk[element] = true;
+//			q.push(element);
+//		}
+//	}
+//
+//	while (q.size()) {
+//		int f = q.front();
+//		ans++;
+//		q.pop();
+//		for (auto element : grp[f]) {
+//			if (!chk[element]) {
+//				chk[element] = true;
+//				q.push(element);
+//			}
+//		}
+//	}
+//}
+//
+//int main() {
+//	cin >> n;
+//	cin >> m;
+//	
+//	int f, s;
+//	for (int i = 0; i < m; i++) {
+//		cin >> f >> s;
+//		grp[f].push_back(s);
+//		grp[s].push_back(f);
+//	}
+//
+//	bfs(1);
+//
+//	cout << ans;
+//
+//	return 0;
+//}
