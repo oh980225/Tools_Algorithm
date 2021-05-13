8c029439cf89ba1262d84b33cd01c733178d45e4
oh980225 <oh980225@gmail.com>
2021-05-13 22:48:43 +0900

    Solve 2206, 네트워크(programmers)

--- Programmers/Programmers/타겟 넘버.cpp
+++ Programmers/Programmers/타겟 넘버.cpp
@@ -1,48 +1,48 @@
-#include <string>
-#include <vector>
-#include <iostream>
-
-using namespace std;
-
-vector<int> v;
-int t;
-int ans;
-bool check[22];
-
-void dfs(int idx, int total) {
-    if (total == t) {
-        ans++;
-        return;
-    }
-    for (int i = idx; i < v.size(); i++) {
-        if (!check[i]) {
-            check[i] = true;
-            v[i] *= (-1);
-            dfs(i, total + 2 * v[i]);
-            v[i] *= (-1);
-            check[i] = false;
-        }
-    }
-}
-
-int solution(vector<int> numbers, int target) {
-    int answer = 0;
-    v = numbers;
-    t = target;
-    int total = 0;
-
-    for (auto number : numbers) {
-        total += number;
-    }
-
-    dfs(0, total);
-    answer = ans;
-
-    return answer;
-}
-
-int main() {
-    vector<int> numbers = { 1, 3, 5, 8, 1 };
-    cout << solution(numbers, 2);
-    return 0;
-}
+//#include <string>
+//#include <vector>
+//#include <iostream>
+//
+//using namespace std;
+//
+//vector<int> v;
+//int t;
+//int ans;
+//bool check[22];
+//
+//void dfs(int idx, int total) {
+//    if (total == t) {
+//        ans++;
+//        return;
+//    }
+//    for (int i = idx; i < v.size(); i++) {
+//        if (!check[i]) {
+//            check[i] = true;
+//            v[i] *= (-1);
+//            dfs(i, total + 2 * v[i]);
+//            v[i] *= (-1);
+//            check[i] = false;
+//        }
+//    }
+//}
+//
+//int solution(vector<int> numbers, int target) {
+//    int answer = 0;
+//    v = numbers;
+//    t = target;
+//    int total = 0;
+//
+//    for (auto number : numbers) {
+//        total += number;
+//    }
+//
+//    dfs(0, total);
+//    answer = ans;
+//
+//    return answer;
+//}
+//
+//int main() {
+//    vector<int> numbers = { 1, 3, 5, 8, 1 };
+//    cout << solution(numbers, 2);
+//    return 0;
+//}
--- Week3/Week3/10026.cpp
+++ Week3/Week3/10026.cpp
@@ -1,59 +1,59 @@
-#include <bits/stdc++.h>
-
-using namespace std;
-
-int n, ans1, ans2;
-vector<string> rgb;
-bool check[105][105];
-struct POS {
-	int y, x;
-};
-POS moveArr[4] = { {0, 1}, {1, 0}, {-1, 0} ,{0, -1} };
-
-void dfs(int y, int x, char pre) {
-	for (int i = 0; i < 4; i++) {
-		int nY = y + moveArr[i].y;
-		int nX = x + moveArr[i].x;
-		if (0 <= nY && nY < n && 0 <= nX && nX < n && !check[nY][nX] && pre == rgb[nY][nX]) {
-			if (rgb[nY][nX] == 'G')
-				rgb[nY][nX] = 'R';
-			check[nY][nX] = true;
-			dfs(nY, nX, pre);
-		}
-	}
-}
-
-int main() {
-	cin >> n;
-	rgb.resize(n);
-	for (int i = 0; i < n; i++)
-		cin >> rgb[i];
-
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			if (!check[i][j]) {
-				ans1++;
-				check[i][j] = true;
-				dfs(i, j, rgb[i][j]);
-				if (rgb[i][j] == 'G')
-					rgb[i][j] = 'R';
-			}
-		}
-	}
-
-	memset(check, 0, sizeof(check));
-
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			if (!check[i][j]) {
-				ans2++;
-				check[i][j] = true;
-				dfs(i, j, rgb[i][j]);
-			}
-		}
-	}
-
-	cout << ans1 << " " << ans2;
-
-	return 0;
-}
+//#include <bits/stdc++.h>
+//
+//using namespace std;
+//
+//int n, ans1, ans2;
+//vector<string> rgb;
+//bool check[105][105];
+//struct POS {
+//	int y, x;
+//};
+//POS moveArr[4] = { {0, 1}, {1, 0}, {-1, 0} ,{0, -1} };
+//
+//void dfs(int y, int x, char pre) {
+//	for (int i = 0; i < 4; i++) {
+//		int nY = y + moveArr[i].y;
+//		int nX = x + moveArr[i].x;
+//		if (0 <= nY && nY < n && 0 <= nX && nX < n && !check[nY][nX] && pre == rgb[nY][nX]) {
+//			if (rgb[nY][nX] == 'G')
+//				rgb[nY][nX] = 'R';
+//			check[nY][nX] = true;
+//			dfs(nY, nX, pre);
+//		}
+//	}
+//}
+//
+//int main() {
+//	cin >> n;
+//	rgb.resize(n);
+//	for (int i = 0; i < n; i++)
+//		cin >> rgb[i];
+//
+//	for (int i = 0; i < n; i++) {
+//		for (int j = 0; j < n; j++) {
+//			if (!check[i][j]) {
+//				ans1++;
+//				check[i][j] = true;
+//				dfs(i, j, rgb[i][j]);
+//				if (rgb[i][j] == 'G')
+//					rgb[i][j] = 'R';
+//			}
+//		}
+//	}
+//
+//	memset(check, 0, sizeof(check));
+//
+//	for (int i = 0; i < n; i++) {
+//		for (int j = 0; j < n; j++) {
+//			if (!check[i][j]) {
+//				ans2++;
+//				check[i][j] = true;
+//				dfs(i, j, rgb[i][j]);
+//			}
+//		}
+//	}
+//
+//	cout << ans1 << " " << ans2;
+//
+//	return 0;
+//}
