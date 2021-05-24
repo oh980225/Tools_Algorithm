7144acf5659d7006638252e9c7cbe1d9c07caaee
oh980225 <oh980225@gmail.com>
2021-05-25 00:17:52 +0900

    Solve 여행경로(programmers)

--- Programmers/Programmers/단어 변환.cpp
+++ Programmers/Programmers/단어 변환.cpp
@@ -1,55 +1,55 @@
-#include <string>
-#include <vector>
-#include <queue>
-#include <map>
-#include <iostream>
-
-using namespace std;
-
-int check[55];
-map<string, int> m;
-
-int bfs(string begin, string target, vector<string> &words) {
-    queue<string> q;
-    q.push(begin);
-    check[m[begin]] = 1;
-    while (q.size()) {
-        auto f = q.front();
-        q.pop();
-        if (f == target)
-            return check[m[target]];
-        for (int i = 0; i < words.size(); i++) {
-            int diff = 0;
-            for (int j = 0; j < words[i].size(); j++) 
-                if (f[j] != words[i][j])
-                    diff++;
-            if (!check[m[words[i]]] && diff < 2) {
-                check[m[words[i]]] = check[m[f]] + 1;
-                q.push(words[i]);
-            }
-        }
-    }
-    return 1;
-}
-
-int solution(string begin, string target, vector<string> words) {
-    int answer = 0;
-    auto it = find(words.begin(), words.end(), target);
-    if (it == words.end()) {
-        return 0;
-    }
-    int i = 0;
-    m[begin] = i++;
-    for (auto element : words)
-        m[element] = i++;
-    m[target] = i;
-    answer = bfs(begin, target, words) - 1;
-
-    return answer;
-}
-
-int main() {
-    vector<string> words = { "hot", "dot", "dog", "lot", "log" };
-    cout << solution("hit", "cog", words);
-    return 0;
-}
+//#include <string>
+//#include <vector>
+//#include <queue>
+//#include <map>
+//#include <iostream>
+//
+//using namespace std;
+//
+//int check[55];
+//map<string, int> m;
+//
+//int bfs(string begin, string target, vector<string> &words) {
+//    queue<string> q;
+//    q.push(begin);
+//    check[m[begin]] = 1;
+//    while (q.size()) {
+//        auto f = q.front();
+//        q.pop();
+//        if (f == target)
+//            return check[m[target]];
+//        for (int i = 0; i < words.size(); i++) {
+//            int diff = 0;
+//            for (int j = 0; j < words[i].size(); j++) 
+//                if (f[j] != words[i][j])
+//                    diff++;
+//            if (!check[m[words[i]]] && diff < 2) {
+//                check[m[words[i]]] = check[m[f]] + 1;
+//                q.push(words[i]);
+//            }
+//        }
+//    }
+//    return 1;
+//}
+//
+//int solution(string begin, string target, vector<string> words) {
+//    int answer = 0;
+//    auto it = find(words.begin(), words.end(), target);
+//    if (it == words.end()) {
+//        return 0;
+//    }
+//    int i = 0;
+//    m[begin] = i++;
+//    for (auto element : words)
+//        m[element] = i++;
+//    m[target] = i;
+//    answer = bfs(begin, target, words) - 1;
+//
+//    return answer;
+//}
+//
+//int main() {
+//    vector<string> words = { "hot", "dot", "dog", "lot", "log" };
+//    cout << solution("hit", "cog", words);
+//    return 0;
+//}
---
+++ Week9/Week9/1613.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n, k;
+
+int matrix[401][401];
+
+int main() {
+	cin >> n >> k;
+	int f, s;
+	for (int i = 0; i < k; i++) { 
+		cin >> f >> s;
+		matrix[f][s] = -1;
+		matrix[s][f] = 1;
+	}
+
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++) {
+			for (int k = 1; k <= n; k++) {
+				if (j == k || k == i || j == i)
+					continue;
+				if (!matrix[j][k]) {
+					if (matrix[j][i] == 1 && matrix[i][k] == 1) {
+						matrix[j][k] = 1;
+					}
+					else if (matrix[j][i] == -1 && matrix[i][k] == -1) {
+						matrix[j][k] = -1;
+					}
+				}
+			}
+		}
+	}
+
+	int cnt;
+	cin >> cnt;
+	for (int i = 0; i < cnt; i++) {
+		cin >> f >> s;
+		cout << matrix[f][s] << endl;
+	}
+	
+
+	return 0;
+}
