4db76d8bc2404a33ba2dcd7a692e44b32e5312fa
oh980225 <oh980225@gmail.com>
2022-03-22 22:05:24 +0900

    Solve 최솟값 만들기(programmers)

---
+++ MyAlgor/MyAlgor/Programmers_MinNum.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool compare(int a, int b) {
+    return a > b;
+}
+
+int solution(vector<int> A, vector<int> B)
+{
+    int answer = 0;
+
+    sort(A.begin(), A.end());
+    sort(B.begin(), B.end(), compare);
+    
+    for(int i = 0; i < A.size(); i++) {
+        answer += (A[i] * B[i]);
+    }
+
+    return answer;
+}
