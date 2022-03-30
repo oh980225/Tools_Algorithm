03fc3e93bed3970b70bc80ee45be7fd5ea03b1ba
oh980225 <oh980225@gmail.com>
2022-03-30 17:02:45 +0900

    Solve 없는 숫자 더하기(programmers)

---
+++ MyAlgor/MyAlgor/Programmers_NoExistNumber.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solution(vector<int> numbers) {
+  int answer = 45;
+    
+  for(int number : numbers) {
+    answer -= number;
+  }
+    
+  return answer;
+}
